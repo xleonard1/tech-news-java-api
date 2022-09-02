c35763314946c11dfc9eff55cfec03411a14bb4d
xleonard1 <xavier.lnrd@gmail.com>
2022-09-01 20:29:41 -0400

    practiced variable calling in Java

---
+++ tech-news-java-api/src/main/java/com/technews/VariablePractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+
+        int a = 20;
+        double b = 30.50;
+        String c = "cat";
+
+        double sum = a + b;
+        String concat = a + c;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+
+}
